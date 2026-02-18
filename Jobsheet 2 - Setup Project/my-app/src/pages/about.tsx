9458c0fda3845b869f474553fc555eb36108e254
Wahib <nurwahibzakkiy@gmail.com>
2026-02-18 10:13:04 +0700

    Jobsheet 2 - Setup Project

---
+++ Jobsheet 2 - Setup Project/my-app/src/pages/about.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <a href="/">Home</a>
+      <h1>Praktikum Next.js Pages Router</h1>
+      <p>Nama : Nurwahib Zakki Yahya</p>
+      <p>NIM : 2341720154</p>
+      <p>Program Studi :D4 Teknik Informatika</p>
+    </div>
+  );
+}
