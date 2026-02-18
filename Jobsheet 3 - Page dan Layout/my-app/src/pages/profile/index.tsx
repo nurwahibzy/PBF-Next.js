7426221c8bd3be06bff1cca87269af2954d44bd2
Wahib <nurwahibzakkiy@gmail.com>
2026-02-18 21:41:05 +0700

    Tugas 1 - routing

---
+++ Jobsheet 3 - Page dan Layout/my-app/src/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+  return (
+    <main>
+      <div>
+        <h1>Profile</h1><br />
+        <h3>Nama : Nurwahib Zakki Yahya</h3> <br />
+        <h3>Program Studi : D4 Teknik Informatika</h3>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
