1afd3af5de81755601ede3e642dc78eaa4eab710
Wahib <nurwahibzakkiy@gmail.com>
2026-02-18 16:35:11 +0700

    Langkah 5 - membuat komponen navbar

--- Jobsheet 3 - Page dan Layout/my-app/src/pages/index.tsx
+++ Jobsheet 3 - Page dan Layout/my-app/src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-        <a href="/about">About</a>
+        {/* <a href="/about">About</a> */}
         <h1>Praktikum Next.js Pages Router</h1>
         <p>Mahasiswa D4 Pengembangan Web</p>
       </div>
