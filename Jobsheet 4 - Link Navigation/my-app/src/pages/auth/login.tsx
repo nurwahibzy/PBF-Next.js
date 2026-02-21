bdb0b5df3c65a71ad578c4e470c3a8303c3164eb
Wahib <nurwahibzakkiy@gmail.com>
2026-02-21 11:24:09 +0700

    Langkah 7 – Navigasi Imperatif (router.push)

--- Jobsheet 4 - Link Navigation/my-app/src/pages/auth/login.tsx
+++ Jobsheet 4 - Link Navigation/my-app/src/pages/auth/login.tsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const halamanLogin = () => {
+  const { push } = useRouter();
+  const handlerLogin = () => {
+    // logic login disini
+    push("/produk");
+  };
   return (
     <div>
       <h1>Halaman Login</h1>
+      <button onClick={handlerLogin}>Login 1</button>
+      <br />
+      <button onClick={() => push("/produk")}>Login 2</button>
+      <br />
+      <button onClick={() => handlerLogin()}>login 3</button>
+      <br />
       <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
