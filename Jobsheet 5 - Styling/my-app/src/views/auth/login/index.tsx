f4b38856cc7290f93ad257c442a16d48f5772d4e
Wahib <nurwahibzakkiy@gmail.com>
2026-02-27 11:17:03 +0700

    Langkah 8 - SCSS (SASS)

--- Jobsheet 5 - Styling/my-app/src/views/auth/login/index.tsx
+++ Jobsheet 5 - Styling/my-app/src/views/auth/login/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./login.module.css";
+// import styles from "./login.module.css";
+import styles from "./login.module.scss";
 
 const halamanLogin = () => {
   const { push } = useRouter();
