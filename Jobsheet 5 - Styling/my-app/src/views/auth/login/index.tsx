39b3ceaa8ed28412a7fb78319eeafd555e97dd7b
Wahib <nurwahibzakkiy@gmail.com>
2026-02-27 12:59:00 +0700

    Langkah 9 - Tailwind CSS

--- Jobsheet 5 - Styling/my-app/src/views/auth/login/index.tsx
+++ Jobsheet 5 - Styling/my-app/src/views/auth/login/index.tsx
@@ -11,7 +11,8 @@ const halamanLogin = () => {
   };
   return (
     <div className={styles.login}>
-      <h1>Halaman Login</h1>
+      {/* <h1>Halaman Login</h1> */}
+      <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1> 
       <br />
       <button onClick={() => handlerLogin()}>login</button>
       <br />
