d6792b006b4f1699bd14d1cc4230d63531b26eae
Wahib <nurwahibzakkiy@gmail.com>
2026-02-28 22:16:18 +0700

    Tugas 3 - Menerapkan minimal 5 utility class Tailwind CSS

--- Jobsheet 5 - Styling/my-app/src/views/produk/produkHero.tsx
+++ Jobsheet 5 - Styling/my-app/src/views/produk/produkHero.tsx
@@ -1,10 +1,10 @@
 const ProductHero = () => {
-    return (
-        <section style={{ padding: '40px', backgroundColor: '#f3f4f6', textAlign: 'center' }}>
-            <h1>Halaman Produk Kami</h1>
-            <p style={{opacity: '50%'}}>Ini adalah Hero section</p>
-        </section>
-    );
+  return (
+    <section className="p-10 bg-gray-100 text-center">
+      <h1 className="text-2xl font-bold mb-2">Halaman Produk Kami</h1>
+      <p className="opacity-50">Ini adalah Hero section</p>
+    </section>
+  );
 };
 
 export default ProductHero;
--- Jobsheet 5 - Styling/my-app/src/views/produk/produkMain.tsx
+++ Jobsheet 5 - Styling/my-app/src/views/produk/produkMain.tsx
@@ -1,14 +1,14 @@
 const ProductMain = ({ id }: { id: string | string[] | undefined }) => {
-  return id && id !== undefined && id !== null ? (
-    <section style={{ padding: "20px", textAlign: "center" }}>
-      <p>Produk: {id}</p>
-      <p style={{ opacity: "50%" }}>Ini adalah Main section</p>
+  // return id && id !== undefined && id !== null ? (
+  // Kondisi tersebut bisa disederhanakan menjadi id saja
+  return id ? (
+    <section className="p-5 text-center">
+      <p className="text-lg font-medium">Produk: {id}</p>
+      <p className="opacity-50 mt-2">Ini adalah Main section</p>
     </section>
   ) : (
-    <section
-      style={{ padding: "20px", textAlign: "center", backgroundColor: "#3333" }}
-    >
-      <p style={{ opacity: "50%" }}>Ini adalah Main section</p>
+    <section className="p-5 text-center bg-black/20">
+      <p className="opacity-50">Ini adalah Main section</p>
     </section>
   );
 };
