29bfacd899c984f2f82e2c868ca2153106834207
Wahib <nurwahibzakkiy@gmail.com>
2026-03-01 21:35:57 +0700

    Tugas 1 - Menambahkan judul, deskripsi, dan ilustrasi pada halaman 404

--- Jobsheet 6 - Custom Error dan document/my-app/src/pages/404.tsx
+++ Jobsheet 6 - Custom Error dan document/my-app/src/pages/404.tsx
@@ -1,13 +1,18 @@
 import styles from "../styles/404.module.scss";
+import Head from "next/head";
 const Custom404 = () => {
   return (
     <div className={styles.error}>
-      <img
-        src="/page-not-found.png"
-        alt="404"
-        className={styles.error__image}
-      />
-      <h1>404 - Halaman Tidak Ditemukan</h1>
+      <Head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+        <meta
+          name="description"
+          content="Maaf, halaman yang Anda cari tidak tersedia."
+        />
+      </Head>
+      <img src="/404.jpg" alt="404" className={styles.error__image} />
+      <h1>404</h1>
+      <h2>Halaman Tidak Ditemukan</h2>
       <p>Maaf, halaman yang Anda cari tidak ada.</p>
     </div>
   );
