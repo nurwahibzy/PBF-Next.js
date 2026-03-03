9920eda614b26d5b9009eb8cca343eab92701156
Wahib <nurwahibzakkiy@gmail.com>
2026-03-03 12:45:02 +0700

    Langkah 3 – Fetch Data API di Frontend

--- Jobsheet 7 - API Routes/my-app/src/pages/produk/index.tsx
+++ Jobsheet 7 - API Routes/my-app/src/pages/produk/index.tsx
@@ -1,22 +1,51 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import ProductView from "../../views/produk"; 
 
-const Produk = () => {
-    const [isLogin, setIsLogin] = useState(true);
-    const { push } = useRouter();
+type ProductType = {
+    id: number;
+    nama: string;
+    harga: number;
+    ukuran: string;
+    warna: string;
+}
+
+const Kategori = () => {
+    // const [isLogin, setIsLogin] = useState(true);
+    // const { push } = useRouter();
+    const [products, setProducts] = useState([]);
+
+    // useEffect(() => {
+    //     if (!isLogin) {
+    //         push("/auth/login");
+    //     }
+    // }, [isLogin, push]); 
+
+    // // mencegah rendering sebelum redirect terjadi 
+    // // agar tidak ada flash dari halaman Kategori
+    // if (!isLogin) return null; 
 
     useEffect(() => {
-        if (!isLogin) {
-            push("/auth/login");
-        }
-    }, [isLogin, push]); 
+        fetch("/api/produk")
+            .then((res) => res.json())
+            .then((data) => setProducts(data.data))
+            .catch((err) => console.error("Error fetching products:", err));
+    }, []);
 
-    // mencegah rendering sebelum redirect terjadi 
-    // agar tidak ada flash dari halaman produk
-    if (!isLogin) return null; 
-
-    return <ProductView id={undefined} />;
+    return (
+        <div>
+            <h1>Daftar Produk</h1>
+            <ul>
+                {products.map((product: ProductType) => (
+                    <li key={product.id}>
+                        {product.nama}
+                            <li>Harga: {product.harga}</li>
+                            <li>Ukuran: {product.ukuran}</li>
+                            <li>Warna: {product.warna}</li>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
-export default Produk;
+export default Kategori;
