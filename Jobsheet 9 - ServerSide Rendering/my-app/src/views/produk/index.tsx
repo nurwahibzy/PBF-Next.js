dda69e7cda44cb2df31da1e9d53e6367896ba380
Wahib <nurwahibzakkiy@gmail.com>
2026-03-04 23:19:39 +0700

    Langkah 1 – Setup Halaman SSR

--- Jobsheet 9 - ServerSide Rendering/my-app/src/views/produk/index.tsx
+++ Jobsheet 9 - ServerSide Rendering/my-app/src/views/produk/index.tsx
@@ -9,54 +9,37 @@ type ProductType = {
   category: string;
 };
 
-const TampilanProduk = ({
-  products,
-  isLoading,
-}: {
-  products: ProductType[];
-  isLoading: boolean;
-}) => {
+const TampilanProduk = ({ products }: { products: ProductType[] }) => {
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
       <div className={styles.produk__content}>
-        {isLoading ? (
-          [1, 2, 3, 4].map((item) => (
-            <div key={item} className={styles.produk__content__item}>
-              <div className={styles.produk__content__item__image}></div>
-              <div className={styles.produk__content__item__name}></div>
-              <div className={styles.produk__content__item__category}></div>
-              <div className={styles.produk__content__item__size}></div>
-              <div className={styles.produk__content__item__price}></div>
-            </div>
-          ))
-        ) : products.length > 0 ? (
+        {products.length > 0 ? (
           <>
-            {products.map((product: ProductType) => (
-              <div key={product.id} className={styles.produk__content__item}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  width={200}
-                  className={styles.produk__content__item__image}
-                />
+            {products.map((products: ProductType) => (
+              <div key={products.id} className={styles.produk__content__item}>
+                <div className={styles.produk__content__item__image}>
+                  <img src={products.image} alt={products.name} width={200} />
+                </div>
                 <h4 className={styles.produk__content__item__name}>
-                  Nama: {product.name}
+                  {products.name}
                 </h4>
                 <p className={styles.produk__content__item__category}>
-                  Kategori: {product.category}
-                </p>
-                <p className={styles.produk__content__item__size}>
-                  Ukuran: {product.size}
+                  {products.category}
                 </p>
                 <p className={styles.produk__content__item__price}>
-                  Rp {product.price.toLocaleString("id-ID")}
+                  Rp {products.price.toLocaleString("id-ID")}
                 </p>
               </div>
-            ))}{" "}
+            ))}
           </>
         ) : (
-          <p>Produk tidak ditemukan.</p>
+          <div className={styles.produk__content__skeleton}>
+            <div className={styles.produk__content__skeleton__image}></div>
+            <div className={styles.produk__content__skeleton__name}></div>
+            <div className={styles.produk__content__skeleton__category}></div>
+            <div className={styles.produk__content__skeleton__price}></div>
+          </div>
         )}
       </div>
     </div>
