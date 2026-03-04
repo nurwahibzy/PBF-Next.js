8cc17169f46b7143c5770ca7c780a01c5376f8c7
Wahib <nurwahibzakkiy@gmail.com>
2026-03-04 13:45:57 +0700

    Langkah 4 - Implementasi SWR

--- Jobsheet 8 - ClientSide Rendering/my-app/src/pages/produk/index.tsx
+++ Jobsheet 8 - ClientSide Rendering/my-app/src/pages/produk/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import TampilanProduk from "@/views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
 
 const Kategori = () => {
   // const [isLogin, setIsLogin] = useState(true);
@@ -9,32 +11,11 @@ const Kategori = () => {
   // state untuk menyimpan data produk yang diambil dari API
   const [products, setProducts] = useState([]);
 
-  // state untuk menandakan apakah data sedang dimuat atau tidak
-
-  // useEffect(() => {
-  //     if (!isLogin) {
-  //         push("/auth/login");
-  //     }
-  // }, [isLogin, push]);
-
-  // // mencegah rendering sebelum redirect terjadi
-  // // agar tidak ada flash dari halaman Kategori
-  // if (!isLogin) return null;
-
-  useEffect(() => {
-    fetch("/api/produk")
-      .then((res) => res.json())
-      .then((data) => {
-        setProducts(data.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-      });
-  }, []);
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
   );
 };
