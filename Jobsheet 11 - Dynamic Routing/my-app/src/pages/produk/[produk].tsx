093b7c3c472523ae9c22c08bd9280e25d332da3e
Wahib <nurwahibzakkiy@gmail.com>
2026-03-09 22:02:03 +0700

    Langkah 3 – Implementasi SSR

--- Jobsheet 11 - Dynamic Routing/my-app/src/pages/produk/[produk].tsx
+++ Jobsheet 11 - Dynamic Routing/my-app/src/pages/produk/[produk].tsx
@@ -3,17 +3,43 @@ import fetcher from "@/utils/swr/fetcher";
 import useSWR from "swr";
 import DetailProduk from "@/views/DetailProduk";
 
+const HalamanProduk = ({ product }: { product: any }) => {
+  // const Router = useRouter();
+  // console.log(Router);
 
-const HalamanProduk = () => {
-    // const Router = useRouter();
-    // console.log(Router);
-    const { query } = useRouter();
-    const { data, error, isLoading } = useSWR(`/api/produk/${query.produk}`, fetcher);
+  {
+    / digunakan untuk client side rendering/;
+  }
+  //     const { query } = useRouter();
+  //     const { data, error, isLoading } = useSWR(`/api/produk/${query.produk}`, fetcher);
+  //   return (
+  //   <div>
+  //     <DetailProduk product={isLoading?[]:data.data}/>
+  //  </div>
+  //     );
+
   return (
-  <div>
-    <DetailProduk product={isLoading?[]:data.data}/>
- </div>
-    );
+    <div>
+      <DetailProduk product={product} />
+    </div>
+  );
 };
 
 export default HalamanProduk;
+
+// fungsi getServerSideProps akan dieksekusi di server setiap kali halaman ini diakses, 
+// dan akan mengambil data produk dari API sebelum halaman dirender. 
+// Data produk yang diambil akan dikirim sebagai props ke komponen HalamanProduk.
+export async function getServerSideProps({
+  params,
+}: {
+  params: { produk: string };
+}) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const data = await res.json();
+  return {
+    props: {
+      product: data.data,
+    },
+  };
+}
