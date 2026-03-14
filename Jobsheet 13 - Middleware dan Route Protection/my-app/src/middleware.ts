e0645421776be5a18bcdc6b87ac2ea91b1573e01
Wahib <nurwahibzakkiy@gmail.com>
2026-03-14 20:29:23 +0700

    Langkah 5 – Simulasi Sistem Login

--- Jobsheet 13 - Middleware dan Route Protection/my-app/src/middleware.ts
+++ Jobsheet 13 - Middleware dan Route Protection/my-app/src/middleware.ts
@@ -2,7 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+  const isLogin = false;
+  if (isLogin) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 }
 
 export const config = {
