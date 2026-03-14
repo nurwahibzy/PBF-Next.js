d9dc454a82559544a20f224173a2cda874576c1e
Wahib <nurwahibzakkiy@gmail.com>
2026-03-14 20:20:02 +0700

    Langkah 4 – Batasi Route Tertentu

--- Jobsheet 13 - Middleware dan Route Protection/my-app/src/middleware.ts
+++ Jobsheet 13 - Middleware dan Route Protection/my-app/src/middleware.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // return NextResponse.next();
-
   return NextResponse.redirect(new URL("/", request.url));
 }
+
+export const config = {
+  matcher: ["/produk", "/about"],
+};
