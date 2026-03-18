1311cad342b6388b2887bace63c6ed12f8feddf2
Wahib <nurwahibzakkiy@gmail.com>
2026-03-18 11:06:14 +0700

    Langkah 5 – Tambahkan Tombol Login & Logout

--- Jobsheet 14 - Sistem Autentikasi dan Proteksi Route/my-app/src/components/layouts/navbar/index.tsx
+++ Jobsheet 14 - Sistem Autentikasi dan Proteksi Route/my-app/src/components/layouts/navbar/index.tsx
@@ -1,10 +1,18 @@
 import styles from "./navbar.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 const Navbar = () => {
+  const {data}  = useSession();
   return (
     <div className={styles.navbar}>
       <div>
         <h2>Navbar</h2>
       </div>
+      {data ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
     </div>
   );
 };
