2fdd741cc2ae4f36ebd3f3ce95390d0da1bc1a5c
Wahib <nurwahibzakkiy@gmail.com>
2026-03-20 05:16:04 +0700

    Jobsheet 15 - init - AuthRegister (databaseIntegration)

---
+++ Jobsheet 15 - AuthRegister (databaseIntegration)/my-app/src/pages/produk/index.tsx
@@ -0,0 +1,22 @@
+  import { useRouter } from "next/router";
+  import { useEffect, useState } from "react";
+  import TampilanProduk from "@/views/produk";
+  import useSWR from "swr";
+  import fetcher from "@/utils/swr/fetcher";
+
+  const Kategori = () => {
+
+    // state untuk menyimpan data produk yang diambil dari API
+    const [products, setProducts] = useState([]);
+
+    const {push} = useRouter();
+
+    const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+
+    return (
+      <div>
+        <TampilanProduk products={isLoading ? [] : data?.data}/>
+      </div>
+    );
+  };
+  export default Kategori;
---
+++ Jobsheet 15 - AuthRegister (databaseIntegration)/my-app/src/views/auth/login/index.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+// import styles from "./login.module.css";
+import styles from "./login.module.scss";
+
+const halamanLogin = () => {
+  const { push } = useRouter();
+  const handlerLogin = () => {
+    // logic login disini
+    push("/produk");
+  };
+  return (
+    <div className={styles.login}>
+      {/* <h1>Halaman Login</h1> */}
+      <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1> 
+      <br />
+      <button onClick={() => handlerLogin()}>login</button>
+      <br />
+      <h1
+        style={{
+          color: "red",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        Belum Punya Akun?
+      </h1>
+      <br />
+      <Link href="/auth/register">Ke Halaman Register</Link>
+    </div>
+  );
+};
+
+export default halamanLogin;
