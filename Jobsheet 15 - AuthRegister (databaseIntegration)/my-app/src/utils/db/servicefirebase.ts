16f715655e75a80c8eadaefc19be7d92b5b74b3f
Wahib <nurwahibzakkiy@gmail.com>
2026-03-20 06:13:10 +0700

    Langkah 2 – Membuat API Register

--- Jobsheet 15 - AuthRegister (databaseIntegration)/my-app/src/utils/db/servicefirebase.ts
+++ Jobsheet 15 - AuthRegister (databaseIntegration)/my-app/src/utils/db/servicefirebase.ts
@@ -5,6 +5,9 @@ import {
   Firestore,
   getDoc,
   doc,
+  addDoc,
+  where,
+  query,
 } from "firebase/firestore";
 import app from "./firebase";
 
@@ -23,4 +26,30 @@ export async function retrieveDataById(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(db, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+  },
+  callback: Function,
+) {
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email),
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (data.length > 0) {
+    callback({ status: "error", message: "User already exists" });
+  } else {
+    // user belum ada, jadi boleh mendaftar
+    callback({ status: "success", message: "User registered successfully" });
+  }
 }
--- Jobsheet 15 - AuthRegister (databaseIntegration)/my-app/src/views/auth/register/index.tsx
+++ Jobsheet 15 - AuthRegister (databaseIntegration)/my-app/src/views/auth/register/index.tsx
@@ -1,12 +1,42 @@
 import Link from "next/link";
 import styles from "./register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const TampilanRegister = () => {
+const [isLoading, setIsLoading] = useState(false);
+const {push} = useRouter();
+const [error,setError] = useState("");
+
+const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+event.preventDefault();
+const form = event.currentTarget;
+const formData = new FormData(event.currentTarget);
+const email = formData.get("email") as string;
+const fullname = formData.get("Fullname") as string;
+const password = formData.get("Password") as string;
+const response = await fetch("/api/register", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ email, fullname, password })
+});
+
+if (response.status === 200) {
+  form.reset();
+  setIsLoading(false);
+  push("/auth/login");
+} else {
+  setIsLoading(false);
+  setError(response.status === 400 ? "User already exists" : "An error occurred");
+}};
+
   return (
     <div className={styles.register}>
       <h1 className={styles.register__title}>Halaman Register</h1>
       <div className={styles.register__form}>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className={styles.register__form__item}>
             <label
               htmlFor="email"
