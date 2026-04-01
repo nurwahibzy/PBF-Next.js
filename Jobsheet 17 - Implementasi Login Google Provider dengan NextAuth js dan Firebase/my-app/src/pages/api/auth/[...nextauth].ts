6a9f5fe7f0dca0c32ed01f2abeb5c52a91f87e91
Wahib <nurwahibzakkiy@gmail.com>
2026-04-01 21:29:10 +0700

    Menyimpan data Google ke Firebase

--- Jobsheet 17 - Implementasi Login Google Provider dengan NextAuth js dan Firebase/my-app/src/pages/api/auth/[...nextauth].ts
+++ Jobsheet 17 - Implementasi Login Google Provider dengan NextAuth js dan Firebase/my-app/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { signIn } from "../../../utils/db/servicefirebase";
+import { signIn, signInWithGoogle } from "../../../utils/db/servicefirebase";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
@@ -63,10 +63,15 @@ export const authOptions: NextAuthOptions = {
           image: user.image,
           type: account.provider,
         };
-        token.fullname = data.fullname;
-        token.email = data.email;
-        token.image = data.image;
-        token.type = data.type;
+        await signInWithGoogle(data, (result: any) => {
+          if (result.status) {
+            token.fullname = result.data.name;
+            token.email = result.data.email;
+            token.image = result.data.image;
+            token.type = result.data.type;
+            token.role = result.data.role;
+          }
+        });
       }
       return token;
     },
--- Jobsheet 17 - Implementasi Login Google Provider dengan NextAuth js dan Firebase/my-app/src/utils/db/servicefirebase.ts
+++ Jobsheet 17 - Implementasi Login Google Provider dengan NextAuth js dan Firebase/my-app/src/utils/db/servicefirebase.ts
@@ -8,6 +8,7 @@ import {
   addDoc,
   where,
   query,
+  updateDoc,
 } from "firebase/firestore";
 import app from "./firebase";
 import bcrypt from "bcrypt";
@@ -79,5 +80,42 @@ export async function signUp(
       .catch((error) => {
         callback({ status: "error", message: error.message });
       });
+  }
+}
+
+export async function signInWithGoogle(userData: any, callback: any) {
+  try {
+    const q = query(
+      collection(db, "users"),
+      where("email", "==", userData.email),
+    );
+
+    const querySnapshot = await getDocs(q);
+    const data: any = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    if (data.length > 0) {
+      // user sudah terdaftar maka update data user
+      userData.role = data[0].role; // pastikan role tetap sama
+      await updateDoc(doc(db, "users", data[0].id), userData);
+      callback({
+        status: true,
+        message: "user registered and logged in with Google",
+      });
+    } else {
+      userData.role = "Member";
+      await addDoc(collection(db, "users"), userData);
+      callback({
+        status: true,
+        message: "user successfully registered  with Google",
+      });
+    }
+  } catch (error) {
+    callback({
+      status: false,
+      message: "Failed to register user with Google",
+    });
   }
 }
