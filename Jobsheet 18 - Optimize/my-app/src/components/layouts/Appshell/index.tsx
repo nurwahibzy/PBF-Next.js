80cb5ce867e6b904ccb96a0a436029287ce07974
Wahib <nurwahibzakkiy@gmail.com>
2026-04-02 10:31:35 +0700

    Optimasi font

--- Jobsheet 18 - Optimize/my-app/src/components/layouts/Appshell/index.tsx
+++ Jobsheet 18 - Optimize/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import Navbar from "../navbar";
+import { Roboto } from "next/font/google";
 
 const disableNavbar = ["/auth/login", "/auth/register", "/404"];
 
@@ -7,13 +8,18 @@ type AppShellProps = {
   children: React.ReactNode;
 };
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
   const { pathname } = useRouter();
 
   const showNavbar = !disableNavbar.includes(pathname);
   return (
-    <main>
+    <main className={roboto.className}>
       {showNavbar && <Navbar />}
       {children}
       <br />
