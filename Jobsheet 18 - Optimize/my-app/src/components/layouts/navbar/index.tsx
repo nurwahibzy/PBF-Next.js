a4d94a411bc0d8798ade8ed4ede05e1d0a5af008
Wahib <nurwahibzakkiy@gmail.com>
2026-04-02 10:31:44 +0700

    Optimasi script

--- Jobsheet 18 - Optimize/my-app/src/components/layouts/navbar/index.tsx
+++ Jobsheet 18 - Optimize/my-app/src/components/layouts/navbar/index.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import styles from "./navbar.module.css";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
@@ -7,8 +8,11 @@ const Navbar = () => {
   // console.log("session", session)
   return (
     <div className={styles.navbar}>
-      <div className={styles.navbar__brand}>MyApp</div>
-
+      {/* <div className={styles.navbar__brand}>MyApp</div> */}
+      <div className={styles.navbar__brand} id="title"></div>
+      <Script id="title-script" strategy="lazyOnload">
+        {`document.getElementById('title').innerHTML = 'MyApp';`}
+      </Script>
       <div className={styles.navbar__right}>
         {data ? (
           <>
