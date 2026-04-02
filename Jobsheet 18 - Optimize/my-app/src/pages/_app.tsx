7f23dec39be47a4f68405740cd88bb44ec942738
Wahib <nurwahibzakkiy@gmail.com>
2026-04-02 11:23:37 +0700

    Tugas 3 - Menambahkan script Google Analytics menggunakan next/script

--- Jobsheet 18 - Optimize/my-app/src/pages/_app.tsx
+++ Jobsheet 18 - Optimize/my-app/src/pages/_app.tsx
@@ -3,16 +3,41 @@ import type { AppProps } from "next/app";
 import AppShell from "@/components/layouts/Appshell";
 // import Navbar from '@/components/layouts/navbar'
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
-    </SessionProvider>
+    <>
+      {/* Mengambil library google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      {/* Menginisialisasi Google Analytics */}
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <SessionProvider session={session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+    </>
   );
 }
